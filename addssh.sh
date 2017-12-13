902198687b1136ae9e68ee0b2f2fb11c293651cc
David Hellmann <davidhellmann.com@gmail.com>
2017-12-13 16:38:00 +0100

    Update addssh.sh

--- addssh.sh
+++ addssh.sh
@@ -1,6 +1,6 @@
 # generate ssh Key
 echo "Generate ssh Key"
-ssh-keygen -t rsa -b 4096 -C "davidhellmann.com@gmail.com"
+ssh-keygen -t rsa -b 4096 -C "david@hellmann.io"
 
 # add key to ssh agent
 echo "Add generated Key to ssh-agent"
